c0d02e09c12e0b1f49515137ca547f9937c3cf1c
Ai Ling <chrome7790@gmail.com>
2025-03-05 21:03:56 +0900

    Added sitemap.xml and route

--- app.py
+++ app.py
@@ -11,5 +11,9 @@ def home():
 def google_verification():
     return send_from_directory('.', 'googlebfe4833e952a6934.html')
 
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('.', 'sitemap.xml')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
